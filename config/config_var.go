09243c94611fa1ced8d7c1d70cc7390c518523ce
along <along@corp.im30.net>
2026-07-14 09:53:19 +0800

    modify sso key

--- config/config_var.go
+++ config/config_var.go
@@ -9,10 +9,9 @@ var ALI_SMS_SIGN_NAME = ""
 var ALI_SMS_TEMPLATE_CODE = ""
 
 var SSO_ENABLED = true
-var SSO_BASE_URL = "http://123.249.5.199:82"
-var SSO_APP_KEY = "bf5a75d320c343f7a2536de79d8238a9"
-var SSO_APP_SECRET = "e69a071cb9d54f23ac80eb4a92e912de"
-
+var SSO_BASE_URL = "http://10.6.160.102:82"
+var SSO_APP_KEY = "03754485bd7442a98205e8d90c088471"
+var SSO_APP_SECRET = "5e88cabddb66461187c0516996a39d13"
 func init() {
 	if os.Getenv("ALI_SMS_ACCESS_ID") != "" {
 		ALI_SMS_ACCESS_ID = os.Getenv("ALI_SMS_ACCESS_ID")
